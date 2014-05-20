77460119e080a330152ad844df6bd2328879cbee
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-20 11:57:21 +0400

    Project init

---
+++ src/test/java/easy/MyTest.java
@@ -0,0 +1,14 @@
+package easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyTest {
+
+    @Test
+    public void myTest() {
+        assertTrue(true);
+
+    }
+}
