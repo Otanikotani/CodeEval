436ea27eb8751eee37d43c39574f3249ee4c3cf0
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-20 13:35:02 +0400

    k

---
+++ src/main/java/util/LineReader.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineReader {
+    public List<String> getLines(String[] args) throws IOException {
+        if (args.length > 0) {
+            return Files.readAllLines(Paths.get(args[0]), Charset.defaultCharset());
+        } else {
+            String className = getClass().getSimpleName();
+            String packageName = getClass().getPackage().getName();
+            InputStream is = this.getClass().getResourceAsStream("/" + packageName + "/" + className + "Sample.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+            List<String> result = new ArrayList<String>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                result.add(line);
+            }
+            return result;
+        }
+    }
+}
