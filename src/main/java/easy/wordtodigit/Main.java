5a181ad0f5af3321a5f239d310a87045d0d26396
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-21 15:42:52 +0400

    Added main classes for all easy challenges

---
+++ src/main/java/easy/wordtodigit/Main.java
@@ -0,0 +1,47 @@
+package easy.wordtodigit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main wordtodigit = new Main();
+        wordtodigit.solve(args);
+        return;
+    }
+
+    public void solve(String[] args) throws IOException {
+        List<String> lines = getLines(args);
+        for (String line: lines) {
+            System.out.println(line);
+        }
+    }
+
+
+    public List<String> getLines(String[] args) throws IOException {
+        if (args.length > 0) {
+            return Files.readAllLines(Paths.get(args[0]), Charset.defaultCharset());
+        } else {
+            String className = getClass().getSimpleName();
+            String packageName = getClass().getPackage().getName();
+            packageName = packageName.replaceAll(Pattern.quote("."), "/");
+            InputStream is = this.getClass().getResourceAsStream("/" + packageName + "/" + className + "Sample.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+            List<String> result = new ArrayList<String>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                result.add(line);
+            }
+            return result;
+        }
+    }
+}
