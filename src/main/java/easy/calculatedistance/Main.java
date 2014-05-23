bde8776b1686f9966e841df197842e32e3c83301
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-24 00:42:02 +0400

    Calculate distance

--- src/main/java/easy/calculatedistance/Main.java
+++ src/main/java/easy/calculatedistance/Main.java
@@ -1,15 +1,15 @@
 package easy.calculatedistance;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.nio.charset.Charset;
+        import java.nio.file.Files;
+        import java.nio.file.Paths;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.regex.Pattern;
 
 public class Main {
 
@@ -22,7 +22,18 @@ public class Main {
     public void solve(String[] args) throws IOException {
         List<String> lines = getLines(args);
         for (String line: lines) {
-            System.out.println(line);
+//            System.out.println(line);
+            String[] points = line.split("\\) \\(");
+            String point1 = points[0];
+            String[] coords1 = point1.substring(1, point1.length()).split(",");
+            int x1 = Integer.parseInt(coords1[0].trim());
+            int y1 = Integer.parseInt(coords1[1].trim());
+
+            String point2 = points[1];
+            String[] coords2 = point2.substring(0, point2.length() - 1).split(",");
+            int x2 = Integer.parseInt(coords2[0].trim());
+            int y2 = Integer.parseInt(coords2[1].trim());
+            System.out.println((int)Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)));
         }
     }
 
