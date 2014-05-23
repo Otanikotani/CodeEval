4532b77ad8203b5e6c17ab30909bef0afd4ed9bf
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-23 14:07:05 +0400

    QueryBoard solved

--- src/main/java/easy/queryboard/Main.java
+++ src/main/java/easy/queryboard/Main.java
@@ -13,6 +13,13 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    private int[][] board = new int[256][256];
+    private static final String COL = "SetCol";
+    private static final String ROW = "SetRow";
+    private static final String QUERY_COL = "QueryCol";
+    private static final String QUERY_ROW = "QueryRow";
+
+
     public static void main(String[] args) throws IOException {
         Main queryboard = new Main();
         queryboard.solve(args);
@@ -22,7 +29,37 @@ public class Main {
     public void solve(String[] args) throws IOException {
         List<String> lines = getLines(args);
         for (String line: lines) {
-            System.out.println(line);
+            if (line.startsWith(ROW)) {
+                String[] params = line.substring(ROW.length() + 1).split(" ");
+                int row = Integer.parseInt(params[0]);
+                int value = Integer.parseInt(params[1]);
+                for (int i = 0; i < 256; i++) {
+                    board[row][i] = value;
+                }
+            } else if (line.startsWith(COL)) {
+                String[] params = line.substring(COL.length() + 1).split(" ");
+                int col = Integer.parseInt(params[0]);
+                int value = Integer.parseInt(params[1]);
+                for (int i = 0; i < 256; i++) {
+                    board[i][col] = value;
+                }
+            } else if (line.startsWith(QUERY_COL)) {
+                String[] params = line.substring(QUERY_COL.length() + 1).split(" ");
+                int col = Integer.parseInt(params[0]);
+                int sum = 0;
+                for (int i = 0; i < 256; i++) {
+                    sum += board[i][col];
+                }
+                System.out.println(sum);
+            } else if (line.startsWith(QUERY_ROW)) {
+                String[] params = line.substring(QUERY_ROW.length() + 1).split(" ");
+                int row = Integer.parseInt(params[0]);
+                int sum = 0;
+                for (int i = 0; i < 256; i++) {
+                    sum += board[row][i];
+                }
+                System.out.println(sum);
+            }
         }
     }
 
