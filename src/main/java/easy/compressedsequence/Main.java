bf96c59af9c969440e7f018d4f70779f6fcacde5
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-24 00:22:30 +0400

    Compressed sequence

--- src/main/java/easy/compressedsequence/Main.java
+++ src/main/java/easy/compressedsequence/Main.java
@@ -22,7 +22,32 @@ public class Main {
     public void solve(String[] args) throws IOException {
         List<String> lines = getLines(args);
         for (String line: lines) {
-            System.out.println(line);
+            String[] parts = line.split(" ");
+            List<Integer> ints = new ArrayList<Integer>();
+            for (String part: parts) {
+                ints.add(Integer.parseInt(part));
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String delimiter = "";
+            Integer current = ints.get(0);
+            int count = 0;
+            for (Integer number: ints) {
+                if (number == current) {
+                    count++;
+                } else {
+                    sb.append(delimiter).append(count);
+                    delimiter = " ";
+                    sb.append(delimiter).append(current);
+                    count = 1;
+                    current = number;
+                }
+            }
+
+            sb.append(delimiter).append(count);
+            delimiter = " ";
+            sb.append(delimiter).append(ints.get(ints.size() - 1));
+            System.out.println(sb.toString());
         }
     }
 
