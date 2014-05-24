84784dbce2640001b80089373c77b55815d23bb2
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-24 19:35:14 +0400

    Working experience

--- src/main/java/easy/workingexperience/Main.java
+++ src/main/java/easy/workingexperience/Main.java
@@ -7,22 +7,43 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Main workingexperience = new Main();
-        workingexperience.solve(args);
+        try {
+            workingexperience.solve(args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         return;
     }
 
-    public void solve(String[] args) throws IOException {
+    public void solve(String[] args) throws IOException, ParseException {
         List<String> lines = getLines(args);
         for (String line: lines) {
-            System.out.println(line);
+//            System.out.println(line);
+            String[] periods = line.split(";");
+            long minDate = Long.MAX_VALUE;
+            long maxDate = Long.MIN_VALUE;
+            for (String period: periods) {
+                String[] dates = period.split("-");
+                for (String dateStr: dates) {
+                    Date date = new SimpleDateFormat("MMM yyyy").parse(dateStr.trim());
+                    minDate = Math.min(date.getTime(), minDate);
+                    maxDate = Math.max(date.getTime(), maxDate);
+                }
+            }
+            Calendar minCal = GregorianCalendar.getInstance();
+            minCal.setTimeInMillis(minDate);
+            Calendar maxCal = GregorianCalendar.getInstance();
+            maxCal.setTimeInMillis(maxDate);
+            System.out.println(maxCal.get(Calendar.YEAR) - minCal.get(Calendar.YEAR));
         }
     }
 
