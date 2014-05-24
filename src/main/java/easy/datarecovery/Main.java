929f801ffdfd567b58d8dee264bf0b23c63e8304
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-05-25 00:03:33 +0400

    Data recovery

--- src/main/java/easy/datarecovery/Main.java
+++ src/main/java/easy/datarecovery/Main.java
@@ -8,7 +8,9 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.TreeMap;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -22,7 +24,44 @@ public class Main {
     public void solve(String[] args) throws IOException {
         List<String> lines = getLines(args);
         for (String line: lines) {
-            System.out.println(line);
+            String[] parts = line.split(";");
+            String[] words = parts[0].split(" ");
+            String[] orders = parts[1].split(" ");
+            TreeMap<Integer, String> orderToWords = new TreeMap<Integer, String>();
+            List<Integer> addedOrders = new ArrayList<Integer>();
+            for (int i = 0; i < words.length; i++ ) {
+                if (i > orders.length - 1) {
+                    int missingKey = -1;
+                    Collections.sort(addedOrders);
+                    if (addedOrders.get(0) == 2) {
+                        missingKey = 1;
+                    } else {
+                        int prevOrder = addedOrders.get(0);
+                        for (int index = 1; index < addedOrders.size(); index++) {
+                            if (addedOrders.get(index) != prevOrder + 1) {
+                                missingKey = prevOrder + 1;
+                                break;
+                            }
+                            prevOrder = addedOrders.get(index);
+                        }
+                        if (missingKey == -1) {
+                            missingKey = addedOrders.size() + 1;
+                        }
+                    }
+                    orderToWords.put(missingKey, words[i]);
+                } else {
+                    addedOrders.add(Integer.parseInt(orders[i]));
+                    orderToWords.put(Integer.parseInt(orders[i]), words[i]);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String delimiter = "";
+            for (String word: orderToWords.values()) {
+                sb.append(delimiter).append(word);
+                delimiter = " ";
+            }
+            System.out.println(sb.toString());
         }
     }
 
