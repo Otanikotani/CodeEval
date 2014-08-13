41c7accee9dc5b49f161d8e59fa29a948c0def7f
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-08-13 09:16:12 +0400

    Bat challenge

--- src/main/java/moderate/batschallenge/Main.java
+++ src/main/java/moderate/batschallenge/Main.java
@@ -7,8 +7,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -22,7 +21,40 @@ public class Main {
     public void solve(String[] args) throws IOException {
         List<String> lines = getLines(args);
         for (String line: lines) {
-            System.out.println(line);
+            String[] parts = line.split(" ");
+            int length = Integer.parseInt(parts[0]);
+            int distance = Integer.parseInt(parts[1]);
+            int numberOfBats = Integer.parseInt(parts[2]);
+            Set<Integer> hangingBats = new TreeSet<Integer>();
+            for (int i = 3; i < parts.length; i++ ){
+                hangingBats.add(Integer.parseInt(parts[i]));
+            }
+            if (numberOfBats == 0) {
+                System.out.println((length - 12) / distance  + 1);
+            } else {
+                int answer = 0;
+                Integer hangingBatIndex = 0;
+                Integer position = 6;
+
+                Iterator iterator = hangingBats.iterator();
+
+                if (iterator.hasNext()) {
+                    hangingBatIndex = (Integer) iterator.next();
+                } else {
+                    answer += untilNextBat(hangingBatIndex, position, length, distance);
+                }
+                System.out.println("invalid");
+            }
+        }
+    }
+
+    private int untilNextBat(Integer hangingBatIndex, Integer position, int length, int distance) {
+        if ((hangingBatIndex - position) > distance) {
+            int result = (hangingBatIndex - position) / distance;
+            position = hangingBatIndex;
+            return result;
+        } else {
+            return 0;
         }
     }
 
