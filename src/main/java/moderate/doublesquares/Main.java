bf4f87448d1a199d43a639fbf0bd14eeec01be64
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2014-09-06 09:50:19 +0400

    Trailing String

--- src/main/java/moderate/doublesquares/Main.java
+++ src/main/java/moderate/doublesquares/Main.java
@@ -16,7 +16,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main doublesquares = new Main();
         doublesquares.solve(args);
-        return;
     }
 
     public void solve(String[] args) throws IOException {
